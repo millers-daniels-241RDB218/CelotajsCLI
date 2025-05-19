425e891b093ee33ec60834cb9bdd787f5ff15292
Toms Graudums <toms.graudums@edu.rtu.lv>
2025-05-19 10:08:15 +0300

    HashTable+Node

---
+++ hashtable.py
@@ -0,0 +1,56 @@
+from node import NodeHT
+
+class HashTable:
+    def __init__ (self, capacity):
+        self.capacity = capacity
+        self.size = 0
+        self.table = [None] * capacity
+    
+    def _hash(self, key):
+        return hash(key) % self.capacity
+    
+    def add(self, key, value):
+        index = self._hash(key)
+        
+        if self.table [index] is None:
+            self.table[index] = NodeHT(key, value)
+            self.size += 1
+        else:
+            current = self.table[index]
+            while current:
+                if current.key == key:
+                    current.value = value
+                    return
+                current = current.next
+            new_node = NodeHT (key, value)
+            new_node.next = self.table[index]
+            self.table[index] = new_node
+            self.size += 1
+        
+    def delete(self, key):
+        index = self._hash(key)
+        
+        previous = None
+        current = self.table[index]
+        
+        while current:
+            if current.key == key:
+                if previous:
+                    previous.next = current.next
+                else:
+                    self.table[index] = current.next
+                self.size -= 1
+                return
+        previous = current
+        current = current.next
+
+    def find(self, key):
+        index = self._hash(key)
+        
+        current = self.table[index]
+        while current:
+            if current.key == key:
+                print(current.value)
+                return
+            current = current.next
+        print("not found")
---
+++ node.py
@@ -0,0 +1,10 @@
+class Node:
+    def __init__ (self, value):
+        self.value = value
+        self.next = None
+        
+class NodeHT:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.next = None
