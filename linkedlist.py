ea065697b9397a45d0275308d0e657af428518cb
ElizaAnna <Eliza-Anna.Jansone@edu.rtu.lv>
2025-05-21 15:58:17 +0300

    linkedlist

---
+++ linkedlist.py
@@ -0,0 +1,46 @@
+from node import Node
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, destination):
+        new_node = Node(destination)
+        if not self.head:
+            self.head = new_node
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
+
+    def delete(self, url):
+        current = self.head
+        prev = None
+
+        while current and hash(current.destination.url) != hash(url):
+            prev = current
+            current = current.next
+
+        if not current:
+            print("Atrašanās vieta nav atrasta!")
+            return
+        
+        if prev:
+            prev.next = current.next
+        else:
+            self.head = current.next
+
+    def search(self, url):
+        current = self.head
+        while current:
+            if hash(current.destination.url) == hash(url):
+                return current.destination
+            current = current.next
+        return None
+    
+    def display(self):
+        current = self.head
+        while current:
+            print(current.destination)
+            current = current.next
